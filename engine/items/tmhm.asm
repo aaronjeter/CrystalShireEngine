9608384a89b132d45d3076c69fa0548936b0865a
aaronjeter <aaronjeter1@gmail.com>
2025-04-08 02:24:12 -0400

    Reusable TMs
    Prevent TMs being consumed, equipped, or tossed

--- engine/items/tmhm.asm
+++ engine/items/tmhm.asm
@@ -155,7 +155,6 @@ TeachTMHM:
 
 	ld c, HAPPINESS_LEARNMOVE
 	farcall ChangeHappiness
-	call ConsumeTM
 	jr .learned_move
 
 .nope
@@ -466,27 +465,6 @@ TMHM_PlaySFX_ReadText2:
 	ld de, SFX_READ_TEXT_2
 	call PlaySFX
 	pop de
-	ret
-
-ConsumeTM:
-	call ConvertCurItemIntoCurTMHM
-	ld a, [wTempTMHM]
-	dec a
-	ld hl, wTMsHMs
-	ld b, 0
-	ld c, a
-	add hl, bc
-	ld a, [hl]
-	and a
-	ret z
-	dec a
-	ld [hl], a
-	ret nz
-	ld a, [wTMHMPocketScrollPosition]
-	and a
-	ret z
-	dec a
-	ld [wTMHMPocketScrollPosition], a
 	ret
 
 CountTMsHMs:
