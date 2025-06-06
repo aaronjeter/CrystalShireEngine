58f4ed287586d52aa9ef5425dacdd79f5808c925
aaronjeter <aaronjeter1@gmail.com>
2025-06-06 16:43:06 -0400

    Change nonparticipant battle xp to 25%
    Right shift nonparticipant battle xp a second time, yielding roughly 25% xp rather than 50%

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -2084,9 +2084,11 @@ UpdateBattleStateAndExperienceAfterEnemyFaint:
 	push de
 	call GiveExperiencePoints
 	pop de
-	; give 50% EXP to non-participants	
+	; give 25% EXP to non-participants	
 	ld hl, wEnemyMonBaseExp
+	;Right shift xp amount twice, roughly quartering it
 	srl [hl]
+	srl [hl] 
 
 	ld a, [wBattleParticipantsNotFainted]
 	push af
