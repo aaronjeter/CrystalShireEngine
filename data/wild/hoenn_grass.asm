b6a695280b6e02edaa471621c640ec2969067bc4
aaronjeter <aaronjeter1@gmail.com>
2025-08-21 18:11:47 -0400

    Add Hoenn wildmon encounter data files

---
+++ data/wild/hoenn_grass.asm
@@ -0,0 +1,201 @@
+; Hoenn Pokémon in grass
+
+HoennGrassWildMons:
+
+def_grass_wildmons ROUTE_101
+	db 2 percent, 2 percent, 2 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 1, ZIGZAGOON
+	dbw 1, WURMPLE
+	dbw 1, POOCHYENA
+	dbw 1, ZIGZAGOON
+	dbw 2, ZIGZAGOON
+	dbw 3, POOCHYENA
+	dbw 7, POOCHYENA
+	; day
+	dbw 1, ZIGZAGOON
+	dbw 1, WURMPLE
+	dbw 1, POOCHYENA
+	dbw 1, ZIGZAGOON
+	dbw 2, ZIGZAGOON
+	dbw 3, POOCHYENA
+	dbw 7, POOCHYENA
+	; nite
+	dbw 1, ZIGZAGOON
+	dbw 1, WURMPLE
+	dbw 1, POOCHYENA
+	dbw 1, ZIGZAGOON
+	dbw 2, ZIGZAGOON
+	dbw 3, POOCHYENA
+	dbw 7, POOCHYENA
+	end_grass_wildmons
+
+def_grass_wildmons ROUTE_102
+	db 2 percent, 2 percent, 2 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 1, ZIGZAGOON
+	dbw 1, POOCHYENA
+	dbw 1, SEEDOT
+	dbw 1, LOTAD
+	dbw 1, RALTS
+	dbw 2, RALTS
+	dbw 5, RALTS
+	; day
+	dbw 1, ZIGZAGOON
+	dbw 1, POOCHYENA
+	dbw 1, SEEDOT
+	dbw 1, LOTAD
+	dbw 1, RALTS
+	dbw 2, RALTS
+	dbw 5, RALTS
+	; nite
+	dbw 1, ZIGZAGOON
+	dbw 1, POOCHYENA
+	dbw 1, SEEDOT
+	dbw 1, LOTAD
+	dbw 1, RALTS
+	dbw 2, RALTS
+	dbw 5, RALTS
+	end_grass_wildmons
+
+def_grass_wildmons ROUTE_103
+	db 2 percent, 2 percent, 2 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 1, ZIGZAGOON
+	dbw 1, POOCHYENA
+	dbw 1, WINGULL
+	dbw 2, ZIGZAGOON
+	dbw 3, POOCHYENA
+	dbw 2, WINGULL
+	dbw 5, RALTS
+	; day
+	dbw 1, ZIGZAGOON
+	dbw 1, POOCHYENA
+	dbw 1, WINGULL
+	dbw 2, ZIGZAGOON
+	dbw 3, POOCHYENA
+	dbw 2, WINGULL
+	dbw 5, RALTS
+	; nite
+	dbw 1, ZIGZAGOON
+	dbw 1, POOCHYENA
+	dbw 1, WINGULL
+	dbw 2, ZIGZAGOON
+	dbw 3, POOCHYENA
+	dbw 2, WINGULL
+	dbw 5, RALTS
+	end_grass_wildmons
+
+def_grass_wildmons ROUTE_104
+	db 2 percent, 2 percent, 2 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 1, ZIGZAGOON
+	dbw 1, POOCHYENA
+	dbw 1, WINGULL
+	dbw 2, TAILLOW
+	dbw 3, MARILL
+	dbw 2, MARILL
+	dbw 5, RALTS
+	; day
+	dbw 1, ZIGZAGOON
+	dbw 1, POOCHYENA
+	dbw 1, WINGULL
+	dbw 2, TAILLOW
+	dbw 3, MARILL
+	dbw 2, MARILL
+	dbw 5, RALTS
+	; nite
+	dbw 1, ZIGZAGOON
+	dbw 1, POOCHYENA
+	dbw 1, WINGULL
+	dbw 2, TAILLOW
+	dbw 3, MARILL
+	dbw 2, MARILL
+	dbw 5, RALTS
+	end_grass_wildmons
+
+def_grass_wildmons ROUTE_110
+	db 2 percent, 2 percent, 2 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 1, ELECTRIKE
+	dbw 1, ODDISH
+	dbw 1, GULPIN
+	dbw 2, PLUSLE
+	dbw 2, MINUN
+	dbw 2, WINGULL
+	dbw 5, MANECTRIC
+	; day
+	dbw 1, ELECTRIKE
+	dbw 1, ODDISH
+	dbw 1, GULPIN
+	dbw 2, PLUSLE
+	dbw 2, MINUN
+	dbw 2, WINGULL
+	dbw 5, MANECTRIC
+	; nite
+	dbw 1, ELECTRIKE
+	dbw 1, ODDISH
+	dbw 1, GULPIN
+	dbw 2, PLUSLE
+	dbw 2, MINUN
+	dbw 2, WINGULL
+	dbw 5, MANECTRIC
+	end_grass_wildmons
+
+def_grass_wildmons ROUTE_116
+	db 2 percent, 2 percent, 2 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 1, NINCADA
+	dbw 1, WHISMUR
+	dbw 1, SKITTY
+	dbw 2, ZIGZAGOON
+	dbw 2, TAILLOW
+	dbw 2, WHISMUR
+	dbw 5, DELCATTY
+	; day
+	dbw 1, NINCADA
+	dbw 1, WHISMUR
+	dbw 1, SKITTY
+	dbw 2, ZIGZAGOON
+	dbw 2, TAILLOW
+	dbw 2, WHISMUR
+	dbw 5, DELCATTY
+	; nite
+	dbw 1, NINCADA
+	dbw 1, WHISMUR
+	dbw 1, SKITTY
+	dbw 2, ZIGZAGOON
+	dbw 2, TAILLOW
+	dbw 2, WHISMUR
+	dbw 5, DELCATTY
+	end_grass_wildmons
+
+def_grass_wildmons ROUTE_117
+	db 2 percent, 2 percent, 2 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 1, SURSKIT
+	dbw 1, ROSELIA
+	dbw 1, ODDISH
+	dbw 2, ZIGZAGOON
+	dbw 2, VOLBEAT
+	dbw 2, ILLUMISE
+	dbw 5, ILLUMISE
+	; day
+	dbw 1, SURSKIT
+	dbw 1, ROSELIA
+	dbw 1, ODDISH
+	dbw 2, ZIGZAGOON
+	dbw 2, VOLBEAT
+	dbw 2, ILLUMISE
+	dbw 5, ILLUMISE
+	; nite
+	dbw 1, SURSKIT
+	dbw 1, ROSELIA
+	dbw 1, ODDISH
+	dbw 2, ZIGZAGOON
+	dbw 2, VOLBEAT
+	dbw 2, ILLUMISE
+	dbw 5, ILLUMISE
+	end_grass_wildmons
+
+db -1 ; end
---
+++ data/wild/hoenn_water.asm
@@ -0,0 +1,61 @@
+; Hoenn Pokémon in water
+
+HoennWaterWildMons:
+
+def_water_wildmons ROUTE_102
+	db 2 percent ; encounter rate
+	dbw 1, SURSKIT
+	dbw 1, GOLDEEN
+	dbw 3, MARILL
+	end_water_wildmons
+
+def_water_wildmons ROUTE_103
+	db 2 percent ; encounter rate
+	dbw 1, WINGULL
+	dbw 1, TENTACOOL
+	dbw 3, MUDKIP
+	end_water_wildmons
+
+def_water_wildmons ROUTE_104
+	db 2 percent ; encounter rate
+	dbw 1, WINGULL
+	dbw 1, MARILL
+	dbw 3, MUDKIP
+	end_water_wildmons
+
+def_water_wildmons ROUTE_105
+	db 2 percent ; encounter rate
+	dbw 1, WINGULL
+	dbw 1, MARILL
+	dbw 3, WAILMER
+	end_water_wildmons
+
+def_water_wildmons ROUTE_108
+	db 2 percent ; encounter rate
+	dbw 1, WINGULL
+	dbw 1, TENTACOOL
+	dbw 3, WAILMER
+	end_water_wildmons
+
+def_water_wildmons ROUTE_109
+	db 2 percent ; encounter rate
+	dbw 1, WINGULL
+	dbw 1, TENTACOOL
+	dbw 3, WAILMER
+	end_water_wildmons
+
+def_water_wildmons ROUTE_110
+	db 2 percent ; encounter rate
+	dbw 1, BARBOACH
+	dbw 1, WINGULL
+	dbw 3, CORPHISH
+	end_water_wildmons
+
+def_water_wildmons ROUTE_117
+	db 2 percent ; encounter rate
+	dbw 1, BARBOACH
+	dbw 1, MUDKIP
+	dbw 3, CORPHISH
+	end_water_wildmons
+
+db -1 ; end
