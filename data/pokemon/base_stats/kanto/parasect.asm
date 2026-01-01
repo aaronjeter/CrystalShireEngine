0b14dbc6b9b42fc64b982f2f1f00a4d9ce6fd507
aaronjeter <aaronjeter1@gmail.com>
2025-12-31 21:04:40 -0500

    Re-type Mega Chimecho and parasect

--- data/pokemon/base_stats/kanto/parasect.asm
+++ data/pokemon/base_stats/kanto/parasect.asm
@@ -4,7 +4,7 @@
 	evs  0,   2,   1,   0,   0,   0
 	;   hp  atk  def  spd  sat  sdf
 
-	db BUG, GRASS ; type
+	db GHOST, GRASS ; type
 	db 75 ; catch rate
 	db 128 ; base exp
 	dw TINYMUSHROOM, BIG_MUSHROOM ; items
