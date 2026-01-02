9e08e39ae74208f8cc2434200e1625f49b74d71b
aaronjeter <aaronjeter1@gmail.com>
2026-01-01 21:28:56 -0500

    Remove Ilex cut tree and Mega Scyther from Bug Contest

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -3,9 +3,8 @@ ContestMons:
 	dbwbb 20, CATERPIE,    7, 18
 	dbwbb 20, WEEDLE,      7, 18
 	dbwbb 10, WURMPLE,     9, 18
-	dbwbb 10, SCYTHER2,    9, 18
-	dbwbb  5, BUTTERFREE, 12, 15
-	dbwbb  5, BEEDRILL,   12, 15
+	dbwbb 10, BUTTERFREE, 12, 15
+	dbwbb 10, BEEDRILL,   12, 15
 	dbwbb 10, VENONAT,    10, 16
 	dbwbb 10, YANMA2,     10, 17
 	dbwbb  5, SCYTHER,    13, 14
