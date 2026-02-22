56dcf344e7ae7c0b08d1365d29be11b36e17276f
aaronjeter <aaronjeter1@gmail.com>
2026-02-22 00:35:05 -0500

    Add Petalburg trade and update trade constants

--- maps/hoenn/petalburg/PetalburgHouse1.asm
+++ maps/hoenn/petalburg/PetalburgHouse1.asm
@@ -1,9 +1,18 @@
 	object_const_def
+	const PETALBURGHOUSE1_FRANK
 
 PetalburgHouse1_MapScripts:
 	def_scene_scripts
 
 	def_callbacks	
+
+PetalburgHouse1_Frank:
+	faceplayer
+	opentext
+	trade NPC_TRADE_FRANK
+	waitbutton
+	closetext
+	end
 
 PetalburgHouse1_MapEvents:
 	db 0, 0 ; filler
@@ -17,4 +26,4 @@ PetalburgHouse1_MapEvents:
 	def_bg_events	
 
 	def_object_events
-	
+	object_event  05,  04, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, PetalburgHouse1_Frank, -1
--- maps/hoenn/petalburg/PetalburgHouse2.asm
+++ maps/hoenn/petalburg/PetalburgHouse2.asm
@@ -1,9 +1,25 @@
 	object_const_def
+	const PETALBURGHOUSE2_GRAMPS
 
 PetalburgHouse2_MapScripts:
 	def_scene_scripts
 
 	def_callbacks	
+
+PetalburgHouse2_GrampsScript:
+	jumptextfaceplayer PetalburgHouse2_GrampsText
+
+PetalburgHouse2_GrampsText:
+	text "The gym leader"
+	line "in town now is"
+	cont "named Norman."
+
+	para "He moved here"
+	line "a while back,"
+
+	para "and brought his"
+	line "family along."
+	done
 
 PetalburgHouse2_MapEvents:
 	db 0, 0 ; filler
@@ -17,4 +33,4 @@ PetalburgHouse2_MapEvents:
 	def_bg_events	
 
 	def_object_events
-	
+	object_event  05,  04, SPRITE_GRAMPS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GRAY, OBJECTTYPE_SCRIPT, 0, PetalburgHouse2_GrampsScript, -1
