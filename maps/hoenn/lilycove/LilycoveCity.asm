0c80009596403359d56f24564f388afea2c3e362
aaronjeter <aaronjeter1@gmail.com>
2026-03-20 17:37:47 -0400

    Update Lilycove City

--- maps/hoenn/lilycove/LilycoveCity.asm
+++ maps/hoenn/lilycove/LilycoveCity.asm
@@ -1,5 +1,9 @@
 	object_const_def
 	const LILYCOVECITY_MOSSDEEPGUY
+	const LILYCOVECITY_ESPEON
+	const LILYCOVECITY_WINGULL1
+	const LILYCOVECITY_WINGULL2
+	const LILYCOVECITY_WINGULL3
 
 LilycoveCity_MapScripts:
 	def_scene_scripts
@@ -38,6 +42,15 @@ LilycoveMossdeepGuyText:
 	line "it on your map."
 	done
 
+
+Lilycove_Espeon:
+	cry ESPEON
+	end
+
+Lilycove_Wingull:
+	cry WINGULL
+	end
+
 LilycoveCity_MapEvents:
 	db 0, 0 ; filler
 
@@ -51,4 +64,8 @@ LilycoveCity_MapEvents:
 
 	def_object_events
 	object_event  46,  33, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, LilycoveMossdeepGuyScript, -1
+	object_event  46,  34, SPRITE_ESPEON, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_PURPLE, OBJECTTYPE_SCRIPT, 0, Lilycove_Espeon, -1
+	object_event  66,  08, SPRITE_WINGULL, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Lilycove_Wingull, -1
+	object_event  50,  31, SPRITE_WINGULL, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Lilycove_Wingull, -1
+	object_event  22,  32, SPRITE_WINGULL, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Lilycove_Wingull, -1
 	
