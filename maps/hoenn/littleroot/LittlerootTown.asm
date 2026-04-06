32bc692686bf07af25c5276fd8dff7122d0b2033
aaronjeter <aaronjeter1@gmail.com>
2026-04-06 11:21:27 -0400

    Unlock Saffron for Hoenn start
    This is a failsafe to prevent game-crash issues if a oenn start player tries to fly without having a Johto or Kanto flypoint unlocked.

--- maps/hoenn/littleroot/LittlerootTown.asm
+++ maps/hoenn/littleroot/LittlerootTown.asm
@@ -12,6 +12,7 @@ LittlerootTown_MapScripts:
 
 LittlerootTownFlypointCallback:
 	setflag ENGINE_FLYPOINT_LITTLEROOT
+	setflag ENGINE_FLYPOINT_SAFFRON
 	endcallback
 
 LittlerootTownConditionalWarpScript:
