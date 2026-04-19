069886f594c16afba3854a202d6d246efbe319c4
aaronjeter <aaronjeter1@gmail.com>
2026-04-19 00:32:09 -0400

    Remove unused Beta maps

--- data/maps/blocks.asm
+++ data/maps/blocks.asm
@@ -40,18 +40,7 @@ Route10South_Blocks:
 	INCBIN "maps/Route10South.ablk"
 
 Pokecenter2F_Blocks:
-CinnabarPokecenter2FBeta_Blocks:
-CeruleanPokecenter2FBeta_Blocks:
-Route10Pokecenter2FBeta_Blocks:
-VermilionPokecenter2FBeta_Blocks:
-PewterPokecenter2FBeta_Blocks:
-FuchsiaPokecenter2FBeta_Blocks:
-LavenderPokecenter2FBeta_Blocks:
-CeladonPokecenter2FBeta_Blocks:
-ViridianPokecenter2FBeta_Blocks:
-SaffronPokecenter2FBeta_Blocks:
 	INCBIN "maps/Pokecenter2F.ablk"
-
 Route41_Blocks:
 	INCBIN "maps/Route41.ablk"
 
@@ -113,7 +102,6 @@ Route38_Blocks:
 	INCBIN "maps/Route38.ablk"
 
 OlivineTimsHouse_Blocks:
-OlivineHouseBeta_Blocks:
 OlivinePunishmentSpeechHouse_Blocks:
 OlivineGoodRodHouse_Blocks:
 Route39Farmhouse_Blocks:
@@ -164,7 +152,6 @@ CherrygroveEvolutionSpeechHouse_Blocks:
 Route30BerryHouse_Blocks:
 	INCBIN "maps/House1.ablk"
 
-SafariZoneFuchsiaGateBeta_Blocks:
 Route19FuchsiaGate_Blocks:
 Route43MahoganyGate_Blocks:
 Route43Gate_Blocks:
@@ -827,9 +814,6 @@ VermilionPortPassage_Blocks:
 
 FuchsiaGym_Blocks:
 	INCBIN "maps/kanto/fuchsia/FuchsiaGym.ablk"
-
-SafariZoneBeta_Blocks:
-	INCBIN "maps/SafariZoneBeta.ablk"
 
 UndergroundPath_Blocks:
 	INCBIN "maps/UndergroundPath.ablk"
