c2c024699da229aa996c7c10e39c4c4161919c53
aaronjeter <aaronjeter1@gmail.com>
2026-07-05 17:12:12 -0400

    Fix barboach's typing
    Give Barboach its secondary Ground typing

--- data/pokemon/base_stats/hoenn/barboach.asm
+++ data/pokemon/base_stats/hoenn/barboach.asm
@@ -4,7 +4,7 @@
 	evs  0,   0,   0,   1,   0,   0
 	;   hp  atk  def  spd  sat  sdf
 
-	db WATER, WATER ; type
+	db WATER, GROUND ; type
 	db 190 ; catch rate
 	db 73 ; base exp
 	dw NO_ITEM, NO_ITEM ; items
