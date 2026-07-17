f2c7d7b8c0b592e68f58b118c49b70b1259dceb2
aaronjeter <aaronjeter1@gmail.com>
2026-07-17 15:33:44 -0400

    More levelup group changes
    Move Houndour and Electrike lines to the medium fast levelup group

--- data/pokemon/base_stats/hoenn/electrike.asm
+++ data/pokemon/base_stats/hoenn/electrike.asm
@@ -12,7 +12,7 @@
 	db 20 ; step cycles to hatch
 	INCBIN "gfx/pokemon/electrike/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_SLOW ; growth rate
+	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_GROUND, EGG_GROUND ; egg groups
 
 	; tm/hm learnset
