61b732fcd15566c2993602797f9ff93069970e63
aaronjeter <aaronjeter1@gmail.com>
2026-07-26 03:32:22 -0400

    Add Evergrande Wally event

--- maps/hoenn/evergrande/EvergrandeCity.asm
+++ maps/hoenn/evergrande/EvergrandeCity.asm
@@ -1,9 +1,55 @@
 	object_const_def
+	const EVERGRANDE_WALLY
 
 EvergrandeCity_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+
+Evergrande_Wally:
+	trainer WALLY, WALLY6, EVENT_FOUND_VICTORY_ROAD_WALLY, EvergrandeWallySeenText, EvergrandeWallyBeatenText, 0, .Script
+
+.Script:
+	opentext
+	writetext EvergrandeWallyAfterBattleText
+	waitbutton
+	closetext
+	special FadeOutToBlack
+	disappear EVERGRANDE_WALLY
+	special FadeInFromBlack
+	end
+
+EvergrandeWallySeenText:
+	text "I knew it!"
+	line "I knew you'd"
+	cont "make it!"
+
+	para "This is it!"
+	line "The championship"
+	cont "awaits."
+
+	para "One last match?"
+	line "For all the"
+	cont "marbles?"
+	done
+
+EvergrandeWallyBeatenText:
+	text "Oof!"
+	done
+
+EvergrandeWallyAfterBattleText:
+	text "You're amazing"
+	line "<PLAY_G>."
+
+	para "You're gonna be"
+	line "a great champ."
+
+	para "I'm really"
+	line "proud of you..."
+
+	para "Good luck in"
+	line "there!"
+	done
 
 EvergrandeCity_MapEvents:
 	db 0, 0 ; filler
@@ -18,4 +64,4 @@ EvergrandeCity_MapEvents:
 	def_bg_events	
 
 	def_object_events
-	
+	object_event  18, 20, SPRITE_BUGSY, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 3, Evergrande_Wally, EVENT_FOUND_VICTORY_ROAD_WALLY
