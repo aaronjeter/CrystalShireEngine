60c0926794a21caee69d04004f861b31c128f0fc
aaronjeter <aaronjeter1@gmail.com>
2026-09-13 19:47:05 -0400

    Add Shrine to Mew

--- maps/kanto/safari/SafariZoneNorth.asm
+++ maps/kanto/safari/SafariZoneNorth.asm
@@ -244,6 +244,134 @@ SafariZoneNorthOldAmber:
 SafariZoneNorthFangFossil:
 	itemball FANG_FOSSIL
 
+MewShrineScript:
+	opentext
+	writetext MewShrineEmptyText
+	waitbutton
+
+	loadmenu .ShrineMenuHeader
+	verticalmenu
+	closewindow
+	ifequal 1, .SilverLeaf
+	ifequal 2, .GoldLeaf
+	ifequal 3, .MarvelScale
+
+	sjump .No
+
+.SilverLeaf
+	checkitem SILVER_LEAF
+	iffalse .No
+
+	writetext MewShrineFirstMonText
+	waitbutton
+	yesorno
+	iffalse .No
+
+	takeitem SILVER_LEAF
+	loadmem wPartyMon1DVs+0, HP_MAX_GHOST
+	loadmem wPartyMon1DVs+1, $aa
+	playsound SFX_CAUGHT_MON
+	waitsfx
+	writetext MewShrineAlignedWithGhostsText
+	waitbutton
+	sjump .Done
+
+.GoldLeaf
+	checkitem GOLD_LEAF
+	iffalse .No
+
+	writetext MewShrineFirstMonText
+	waitbutton
+	yesorno
+	iffalse .No
+
+	takeitem GOLD_LEAF
+	loadmem wPartyMon1DVs+0, HP_MAX_GHOST
+	loadmem wPartyMon1DVs+1, $fe
+	playsound SFX_CAUGHT_MON
+	waitsfx
+	writetext MewShrineEtherialBlessingText
+	waitbutton
+	sjump .Done
+
+.MarvelScale
+	checkitem MARVEL_SCALE
+	iffalse .No
+
+	writetext MewShrineFirstMonText
+	waitbutton
+	yesorno
+	iffalse .No
+
+	takeitem MARVEL_SCALE
+	loadmem wPartyMon1DVs+0, HP_MAX_GHOST
+	loadmem wPartyMon1DVs+1, $ff
+	playsound SFX_CAUGHT_MON
+	waitsfx
+	writetext MewShrineMewBlessingText
+	waitbutton
+	sjump .Done	
+
+.No
+	writetext MewShrineNoOfferingText
+	waitbutton
+.Done
+	closetext
+	end
+
+.ShrineMenuHeader:
+	db MENU_BACKUP_TILES ; flags
+	menu_coords 0, 2, 15, TEXTBOX_Y - 1
+	dw .MenuData
+	db 1 ; default option
+
+.MenuData:
+	db STATICMENU_CURSOR ; flags
+	db 3 ; items	
+	db "Silver Leaf@"
+	db "Gold Leaf@"	
+	db "Marvel Scale@"
+
+MewShrineEmptyText:
+	text "The shrine is"
+	line "empty..."
+
+	para "You sense that it"
+	line "awaits an"
+	cont "offering..."
+	done
+
+MewShrineFirstMonText:
+	text "Offer this item"
+	line "for your first"
+	cont "#mon?"
+	done
+
+MewShrineAlignedWithGhostsText:
+	text "Your first #mon"
+	line "has aligned"
+	cont "with Ghosts!"
+	done
+
+MewShrineEtherialBlessingText:
+	text "Your first #mon"
+	line "has recieved the"
+	cont "Etherial Blessing!"
+	done
+
+MewShrineMewBlessingText:
+	text "Your first #mon"
+	line "has recieved a"
+	cont "Blessing from"
+	cont "Mew!"
+	done
+
+MewShrineNoOfferingText:
+	text "You choose not"
+	line "to make an"
+	cont "offering."
+	done
+
 SafariZoneNorth_MapEvents:
 	db 0, 0 ; filler
 
@@ -265,6 +393,7 @@ SafariZoneNorth_MapEvents:
 	def_coord_events
 
 	def_bg_events	
+	bg_event 34, 30, BGEVENT_UP, MewShrineScript
 
 	def_object_events
 	object_event  34, 38, SPRITE_ROCKET, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 2, SafariGrunt3Script, EVENT_BEAT_SAFARI_GRUNT3
