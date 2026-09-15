23ea266a6721aa132b0027fe57e9fd21d093fb75
aaronjeter <aaronjeter1@gmail.com>
2026-09-14 21:41:57 -0400

    Add Diglet DV guy to Diglett Cave

--- maps/kanto/dungeons/DiglettsCave.asm
+++ maps/kanto/dungeons/DiglettsCave.asm
@@ -20,6 +20,81 @@ DiglettsCavePokefanMText:
 	line "shocking."
 	done
 
+DiglettGuyScript:
+	faceplayer
+	opentext
+
+	writetext DiglettGuyOfferServiceText
+	yesorno
+	iffalse .Refused
+	checkitem GOLD_LEAF
+	iffalse .NotEnoughMoney
+
+	loadmem wPartyMon1DVs+0, HP_MAX_GROUND
+	loadmem wPartyMon1DVs+1, $fe
+
+	waitsfx
+	playsound SFX_TRANSACTION
+	takeitem GOLD_LEAF
+	writetext DiglettGuyAfterServiceText
+	waitbutton
+	closetext
+	end
+
+.NotEnoughMoney:
+	writetext DiglettGuyNoMoneyText
+	waitbutton
+	closetext
+	end
+
+.Refused:
+	writetext DiglettGuyRefusedText
+	waitbutton
+	closetext
+	end
+
+DiglettGuyOfferServiceText:
+	text "I love Digletts!"
+	line "Do you?"
+
+	para "I can fill your"
+	line "first #mon"
+	cont "with the power of"
+	cont "the Earth!"
+
+	para "They'll even look"
+	line "more like Diglett!"
+
+	para "I just need a"
+	line "Gold Leaf. Got"
+	cont "one for me?"
+	done
+
+
+DiglettGuyAfterServiceText:
+	text "And...it is"
+	line "complete!"
+
+	para "Your #mon is"
+	line "filled with Hidden"
+	cont "Ground Power."
+
+	para "And has taken on"
+	line "a lovely shade as"
+	cont "well..."
+	done
+	
+DiglettGuyNoMoneyText:
+	text "Yeah, that isn't"
+	line "a Gold Leaf..."
+	done
+
+DiglettGuyRefusedText:
+	text "Oh, alright."
+	line "It isn't for"
+	cont "just anyone."
+	done
+
 DiglettsCave_MapEvents:
 	db 0, 0 ; filler
 
@@ -38,3 +113,4 @@ DiglettsCave_MapEvents:
 
 	def_object_events
 	object_event  3, 31, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, DiglettsCavePokefanMScript, -1
+	object_event 12, 17, SPRITE_FISHING_GURU, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, DiglettGuyScript, -1
